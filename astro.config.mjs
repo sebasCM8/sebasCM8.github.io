a0364e22132bda787e141e26259d63b58a9e5a9a
sebasCM8 <65082637+sebasCM8@users.noreply.github.com>
2026-02-18 10:23:47 -0400

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com/",
+  site: "https://sebasCM8.github.io/",
 
   vite: {
     plugins: [tailwindcss()]
